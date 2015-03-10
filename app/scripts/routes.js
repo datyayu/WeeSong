c396b879e7455d6b7ef85fa01054f50364c18db5
Arturo Coronel <aoitsu3@gmail.com>
2015-03-09 18:30:16 -0700

    Renombrados templates para mejor organizacion

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -3,17 +3,21 @@ angular.module('weesong', [
   'ngRoute'
 ]);
 
+/* Routes */
 angular.module('weesong')
 .config(function ($routeProvider, $locationProvider) {
   $routeProvider
-    .when('/player',   {controller: 'PlayerCtrl',   templateUrl: 'layouts/player.html'  })
-    .when('/playlists', {templateUrl: 'layouts/playlists.html'})
-    .when('/playlists/:playlistID', {controller: 'PlaylistCtrl', templateUrl: 'layouts/playlist_individual.html'})
+    .when('/player', {
+      controller: 'PlayerCtrl',
+      templateUrl: 'layouts/player.html'
+    })
+    .when('/playlists', {
+      templateUrl: 'PlaylistListCtrl',
+      templateUrl: 'layouts/playlists_list.html'
+    })
+    .when('/playlists/:playlistID', {
+      controller: 'PlaylistCtrl', 
+      templateUrl: 'layouts/playlist.html'
+    })
 
-    /* Uncomment on prod */
-    
-    // $locationProvider.html5Mode({
-    //   enabled: true,
-    //   requireBase: false
-    // });
 });
