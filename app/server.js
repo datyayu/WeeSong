a7411b7f8692342a71d5c3dd8419658573f7b20a
Arturo Coronel <aoitsu3@gmail.com>
2015-03-17 03:25:48 -0700

    Agregado port como env var, y modificado un poco el codigo al estilo ES6

--- app/server.js
+++ app/server.js
@@ -1,20 +1,20 @@
+"use strict"
+
 const path    = require('path')
 const express = require('express')
 const app     = express()
-
 
 // Static files
 const publicFiles = path.join(__dirname, 'public')
 app.use(express.static(publicFiles))
 
-
 // Routes
 app.get('*', handleApp)
 
-
 // Start listening
-app.listen(9000, function () {
-  console.log('Listening on port ' + 9000)
+let port = process.env.PORT || 9000
+app.listen(port, function () {
+  console.log('[ Weesong ] - Running on port ' + port)
 })
 
 // Routes
